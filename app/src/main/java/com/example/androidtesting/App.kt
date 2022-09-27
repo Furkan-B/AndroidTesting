531b198e9757e8a76f47a0b14ea403a361220fef
Furkan <furknberber@gmail.com>
2022-09-27 21:41:15 +0300

    added hilt

---
+++ app/src/main/java/com/example/androidtesting/App.kt
@@ -0,0 +1,7 @@
+package com.example.androidtesting
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
--- app/src/main/java/com/example/androidtesting/api/RetrofitAPI.kt
+++ app/src/main/java/com/example/androidtesting/api/RetrofitAPI.kt
@@ -6,7 +6,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface Retrofit {
+interface RetrofitAPI {
 
     @GET("/api/")
     suspend fun imageSearch(
