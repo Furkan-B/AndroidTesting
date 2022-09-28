72dafdb6ce9859be60d417618773f5e28c2b3beb
Furkan <furknberber@gmail.com>
2022-09-29 02:12:32 +0300

    added hilt (dependency injection) testing

--- app/src/androidTest/java/com/example/androidtesting/roobdb/ArtDaoTest.kt
+++ app/src/androidTest/java/com/example/androidtesting/roobdb/ArtDaoTest.kt
@@ -9,31 +9,45 @@ import com.example.androidtesting.roomdb.Art
 import com.example.androidtesting.roomdb.ArtDao
 import com.example.androidtesting.roomdb.ArtDatabase
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
 
 @SmallTest
 @ExperimentalCoroutinesApi
+@HiltAndroidTest
 class ArtDaoTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database: ArtDatabase
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    @Named("testDatabase")
+    lateinit var database: ArtDatabase
+
     private lateinit var dao: ArtDao
 
     @Before
     fun setup() {
 
-        //inMemoryDatabaseBuilder geçici db, test için uygun
+/*        //inMemoryDatabaseBuilder geçici db, test için uygun
         database = Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),
             ArtDatabase::class.java
         ).allowMainThreadQueries().build()
+
+ */
+        hiltRule.inject()
 
         dao = database.artDao()
     }
